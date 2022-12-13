45222202f065ceb00617489a57fe472d8f63dc6a
Alex Waring <alex.waring@starlingbank.com>
2022-12-13 10:22:12 +0000

    Day 12 and 13

---
+++ Day12/day12.py
@@ -0,0 +1,57 @@
+import networkx
+
+heightmap = []
+
+def createGraph(heightmap):
+    graph = networkx.DiGraph()
+    for y in range(len(heightmap)):
+        for x in range(len(heightmap[y])):
+
+            graph.add_node((x, y))
+            adjacent = [(x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1)]
+
+            for adj_x, adj_y in adjacent:
+                if 0 <= adj_x < len(heightmap[y]) and 0 <= adj_y < len(heightmap):
+                    if heightmap[adj_y][adj_x] - heightmap[y][x] <= 1:
+                        graph.add_edge((x, y), (adj_x, adj_y))
+
+    return graph
+
+
+def SPF(start, end):
+    graph = createGraph(heightmap)
+    path = networkx.shortest_path(graph, source=start, target=end)
+    return len(path) - 1
+
+
+with open("/Users/alex.waring/code/AoC2022/Day12/input.txt") as file:
+    for line in file:
+        line = line.strip()
+        row = []
+        for letter in line:
+            row.append(ord(letter) - 96)
+        heightmap.append(row)
+
+for y, row in enumerate(heightmap):
+    for x, value in enumerate(row):
+        if value == (ord("S") - 96):
+            heightmap[y][x] = 0
+            start = (x, y)
+        elif value == (ord("E") - 96):
+            heightmap[y][x] = 26
+            end = (x, y)
+
+path = SPF(start, end)
+print(path)
+paths = [path]
+
+for y, row in enumerate(heightmap):
+    for x, value in enumerate(row):
+        if value == (ord("a") - 96):
+            start = (x, y)
+            try:
+                paths.append(SPF(start, end))
+            except:
+                print("Nope!! You're lost")
+
+print(min(paths))
---
+++ Day13/day13.py
@@ -0,0 +1,71 @@
+import itertools
+import functools
+
+def compare(data):
+    left = data[0]
+    right = data[1]
+
+    if type(left) is int and type(right) is int:
+        if left < right:
+            return True
+        if left > right:
+            return False
+        else:
+            return
+        
+    if type(left) is int:
+        left = [left]
+    if type(right) is int:
+        right = [right]
+
+    if len(left) == 0 and len(right) != 0:
+        return True       
+    elif len(left) != 0 and len(right) == 0:
+        return False 
+    elif len(left) == 0 and len(right) == 0:
+        return 
+
+    recurse = compare([left[0], right[0]])
+    if recurse != None:
+        return recurse
+    else:
+        return compare([left[1:],right[1:]])
+
+def compareTransform(left, right):
+    if compare([left, right]):
+        return -1
+    else:
+        return +1
+        
+with open("/Users/alex.waring/code/AoC2022/Day13/input.txt") as file:
+    
+    data = []
+
+    while True:
+        data_raw = list(itertools.islice(file, 3))
+        if not data_raw:
+            break
+        else:
+            input = []
+            input.append(eval(data_raw[0].strip()))
+            input.append(eval(data_raw[1].strip()))
+            data.append(input)
+
+    correct_order = [
+        i for i in range(1, len(data) + 1)
+        if compare(data[i-1])
+    ]
+    print(sum(correct_order)) # Part 1
+
+    data = []
+
+with open("/Users/alex.waring/code/AoC2022/Day13/input.txt") as file:
+    for line in file:
+        if line.strip() != "":
+            data.append(eval(line))
+    data.append([[2]])
+    data.append([[6]])
+
+    data.sort(key=functools.cmp_to_key(compareTransform))
+
+    print((data.index([[2]]) + 1) * (data.index([[6]]) + 1))
